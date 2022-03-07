c38c2ab78eaff0027c4a1fb2da3b574084776129
Павел <@DESKTOP-3689THH>
2022-03-07 10:52:12 +0300

    Добавьте файлы проекта.

---
+++ qwertyuiop/Program.cs
@@ -0,0 +1,10 @@
+﻿List<int> sad = new List<int>();
+Random random=new Random();
+for(int i = 0; i < 10; i++)
+    sad.Add(random.Next());
+while (true)
+{
+int X=int.Parse(Console.ReadLine());
+    Console.WriteLine(sad[X]);
+
+}
